d4e843434644849844ed037d23d46824d783ea10
Apprentice <apprentice@dbc27.local>
2013-10-28 16:20:54 -0700

    Changed Rakefile

--- db/migrate/20131028142914_create_screens.rb
+++ db/migrate/20131028142914_create_screens.rb
@@ -1,7 +1,8 @@
-class createScreens < ActiveRecord::Migrate
+class CreateScreens < ActiveRecord::Migrate
   def change
     create_table(:screens) do |t|
       t.string :name
+      t.timestamps
     end
   end
 end
