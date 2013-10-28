8791ae3e497c6f1aabddffefabf68380891deb70
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-10-28 15:48:03 -0700

    Made an error go away.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131028185947) do
+ActiveRecord::Schema.define(version: 20131028142914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
